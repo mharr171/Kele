3aff0a3e1a5fc9cb085253e31abf8907ce6474b7
Matthew Harris <mharr171.z@gmail.com>
2018-02-06 18:20:28 -0800

    add ability to get mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,15 +5,13 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    post_response = self.class.post('/sessions', body: { email: email, password: password } )
-    @auth_token = post_response['auth_token']
-    if !@auth_token
-      p "Incorrect credentials, please try again!"
-    end
+    response = self.class.post(api_url('sessions'), body: { email: email, password: password })
+    @auth_token = response['auth_token']
+    p 'Incorrect credentials, please try again!' unless @auth_token
   end
 
   def get_me
-    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get('/users/me', headers: { 'authorization' => @auth_token })
     @user_data = JSON.parse(response.body)
     @user_data.keys.each do |key|
       self.class.send(:define_method, key.to_sym) do
@@ -22,4 +20,15 @@ class Kele
     end
   end
 
+  def get_mentor_availability(mentor_id)
+    @mentor_id = mentor_id
+    response = self.class.get(api_url("/mentors/#{@mentor_id}/student_availability"), headers: { 'authorization' => @auth_token })
+    JSON.parse(response.body).to_a
+  end
+
+  private
+
+  def api_url(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
+  end
 end
