d8b041cf018f78cacfb5796031aae026a74063f3
Matthew Harris <mharr171.z@gmail.com>
2018-02-08 14:27:55 -0800

    fix number of parameters for create_submission

--- lib/checkpoint.rb
+++ lib/checkpoint.rb
@@ -4,7 +4,7 @@ require 'json'
 module Checkpoint
   include HTTParty
 
-  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment = "")
     response = self.class.post(
       '/checkpoint_submissions',
       headers: { 'authorization' => @auth_token },
@@ -13,7 +13,7 @@ module Checkpoint
         assignment_commit_link: assignment_commit_link,
         checkpoint_id: checkpoint_id,
         comment: comment,
-        enrollment_id: enrollment_id
+        enrollment_id: self.get_me["current_enrollment"]["id"]
       }
     )
   end
